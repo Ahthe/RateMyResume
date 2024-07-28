08a0f92e74d8bcc6bbf3bf36acb123147058209b
John Victor Stephen Durairaj <jh312868@dal.ca>
2024-07-27 21:15:17 -0300

    added sparkles

--- app/components/ResumeAnalyzerApp.tsx
+++ app/components/ResumeAnalyzerApp.tsx
@@ -1,52 +1,60 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 import ResumeUploader from './ResumeUploader';
 import ResumeWorth from './ResumeWorth';
 import styles from '../styles/ResumeAnalyzerApp.module.css';
 import { useCompletion } from 'ai/react';
-
+import { SparklesCore } from './ui/sparkles';
 
 const ResumeAnalyzerApp = () => {
-    const [showWorth, setShowWorth] = useState(false);
-    const [isLoadingResume, setIsLoadingResume] = useState(false);
-    const [resumeText, setResumeText] = useState<string>('');
-    const { completion, isLoading, complete, error } = useCompletion({
-      api: '/api/resume',
-    });
-  
-    useEffect(() => {
-        const getResumeWorth = async (text: string) => {
-          const messageToSend = `RESUME: ${text}\n\n-------\n\n`;
-          await complete(messageToSend);
-          setShowWorth(true);
-          setIsLoadingResume(false);
-        };
-    
-        if (resumeText !== '') {
-          getResumeWorth(resumeText).then();
-        }
-      }, [resumeText]);
+  const [showWorth, setShowWorth] = useState(false);
+  const [isLoadingResume, setIsLoadingResume] = useState(false);
+  const [resumeText, setResumeText] = useState<string>('');
+  const { completion, isLoading, complete, error } = useCompletion({
+    api: '/api/resume',
+  });
 
-      return (
-        <div className={styles.analyzerWrapper}>
-          {!showWorth ? (
-            <div className={styles.uploaderWrapper}>
-              <p className={styles.instructionsText}>Upload your resume to know your rate.</p>
-              <ResumeUploader setIsLoading={setIsLoadingResume} setResumeText={setResumeText} />
-              {(isLoadingResume || isLoading) && 
-                <div className={styles.loadingContainer}>
-                  <div className={styles.loadingSpinner}></div>
-                </div>}
-            </div>
-          ) : (
-            <ResumeWorth resumeWorth={completion} />
-          )}
-          {error && <p className={styles.errorMessage}>{error.message}</p>}
-         
-        </div>
-      );
-  
-    
-  };
+  useEffect(() => {
+    const getResumeWorth = async (text: string) => {
+      const messageToSend = `RESUME: ${text}\n\n-------\n\n`;
+      await complete(messageToSend);
+      setShowWorth(true);
+      setIsLoadingResume(false);
+    };
 
-  
+    if (resumeText !== '') {
+      getResumeWorth(resumeText).then();
+    }
+  }, [resumeText]);
+
+  return (
+    <div className={styles.analyzerWrapper}>
+      <div className="relative w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="absolute inset-0 w-full h-full"
+          particleColor="#FFFFFF"
+        />
+        {!showWorth ? (
+          <div className={styles.uploaderWrapper}>
+            <p className={styles.instructionsText}>Upload your resume to know your rate.</p>
+            <ResumeUploader setIsLoading={setIsLoadingResume} setResumeText={setResumeText} />
+            {(isLoadingResume || isLoading) &&
+              <div className={styles.loadingContainer}>
+                <div className={styles.loadingSpinner}></div>
+              </div>}
+          </div>
+        ) : (
+          <ResumeWorth resumeWorth={completion} />
+        )}
+        {error && <p className={styles.errorMessage}>{error.message}</p>}
+      </div>
+    </div>
+  );
+};
+
 export default ResumeAnalyzerApp;
--- app/components/ResumeUploader.tsx
+++ app/components/ResumeUploader.tsx
@@ -1,138 +1,158 @@
+"use client";
 import React, { useState } from 'react';
 import type { TextContent, TextItem } from 'pdfjs-dist/types/src/display/api';
-import styles from '../styles/ResumeUploader.module.css';
 import { MdCloudUpload } from "react-icons/md";
+import { SparklesCore } from './ui/sparkles';
+import styles from '../styles/ResumeUploader.module.css';
 
 type Props = {
     setResumeText: React.Dispatch<React.SetStateAction<string>>;
     setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
-  };
+};
 
-  const ResumeUploader: React.FC<Props> = ({ setResumeText, setIsLoading }) => {
+const ResumeUploader: React.FC<Props> = ({ setResumeText, setIsLoading }) => {
     const [error, setError] = useState('');
     const [isDragOver, setIsDragOver] = useState(false);
-  
+
     const mergeTextContent = (textContent: TextContent) => {
         return textContent.items
-          .map((item) => {
-            const { str, hasEOL } = item as TextItem;
-            return str + (hasEOL ? '\n' : '');
-          })
-          .join('');
-      };
-  
-      const readResume = async (pdfFile: File | undefined) => {
+            .map((item) => {
+                const { str, hasEOL } = item as TextItem;
+                return str + (hasEOL ? '\n' : '');
+            })
+            .join('');
+    };
+
+    const readResume = async (pdfFile: File | undefined) => {
         const pdfjs = await import('pdfjs-dist');
         pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
-    
+
         if (!pdfFile) return;
-    
+
         const reader = new FileReader();
         reader.onload = async (event) => {
             const arrayBuffer = event.target?.result;
             if (arrayBuffer && arrayBuffer instanceof ArrayBuffer) {
-              const loadingTask = pdfjs.getDocument(new Uint8Array(arrayBuffer));
-              loadingTask.promise.then(
-                (pdfDoc) => {
-                  pdfDoc.getPage(1).then((page) => {
-                    page.getTextContent().then((textContent) => {
-                      const extractedText = mergeTextContent(textContent);
-                      setResumeText(extractedText);
-                    });
-                  });
-                },
-                (reason) => {
-                  console.error(`Error during PDF loading: ${reason}`);
-                }
-              );
+                const loadingTask = pdfjs.getDocument(new Uint8Array(arrayBuffer));
+                loadingTask.promise.then(
+                    (pdfDoc) => {
+                        pdfDoc.getPage(1).then((page) => {
+                            page.getTextContent().then((textContent) => {
+                                const extractedText = mergeTextContent(textContent);
+                                setResumeText(extractedText);
+                            });
+                        });
+                    },
+                    (reason) => {
+                        console.error(`Error during PDF loading: ${reason}`);
+                    }
+                );
             }
-          };
-          reader.readAsArrayBuffer(pdfFile);
-      };
+        };
+        reader.readAsArrayBuffer(pdfFile);
+    };
 
-      const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
+    const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
         setResumeText('');
         setError('');
         setIsLoading(true);
-    
+
         try {
             const items = event.dataTransfer.items;
-      
+
             if (!items || items.length !== 1) {
-              throw new Error('Please drop a single file.');
+                throw new Error('Please drop a single file.');
             }
             const item = items[0];
-      
+
             if (item.kind !== 'file' || item.type !== 'application/pdf') {
-              throw new Error('Please drop a single PDF file.');
+                throw new Error('Please drop a single PDF file.');
             }
             const file = item.getAsFile();
-      
+
             if (!file) {
-              throw new Error("The PDF wasn't uploaded correctly.");
+                throw new Error("The PDF wasn't uploaded correctly.");
             }
             await readResume(file);
-          } catch (error) {
+        } catch (error) {
             setError('There was an error reading the resume. Please try again.');
-          }
-      };
+        } finally {
+            setIsLoading(false);
+            setIsDragOver(false);
+        }
+    };
 
-      const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
         setIsDragOver(true);
-      };
-    
-      const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
+    };
+
+    const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
         setIsDragOver(true);
-      };
-    
-      const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
+    };
+
+    const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
         setIsDragOver(false);
-      };
+    };
 
-      const handleButtonUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleButtonUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
         setError('');
         setIsLoading(true);
         setResumeText('');
-    
-        try {
-          const file = event.target.files?.[0];
-          if (!file) {
-            setError("The PDF wasn't uploaded correctly.");
-            setIsLoading(false);
-            return;
-          }
-          await readResume(file);
-        } catch (error) {
-          setError('There was an error reading the resume. Please try again.');
-        }
-      };
 
-      return (
-        <div>
-          <div
-            className={`${styles.fileUploadBtnContainer} ${isDragOver ? styles.dragOver : ''}`}
-            onDrop={(e: React.DragEvent<HTMLDivElement>) => handleDrop(e)}
-            onDragOver={(e: React.DragEvent<HTMLDivElement>) => handleDragOver(e)}
-            onDragEnter={(e: React.DragEvent<HTMLDivElement>) => handleDragEnter(e)}
-          >
-            <input
-              type="file"
-              id="file-upload"
-              onChange={handleButtonUpload}
-              accept="application/pdf"
-              hidden
-            />
-            <label htmlFor="file-upload" className={`${styles.label} ${styles.mainBtn}`}>
-              <MdCloudUpload /> Upload resume
-            </label>
-          </div>
-          {error && <p className={styles.errorMessage}>{error}</p>}
-        </div>
-      );
-  };
-  
+        try {
+            const file = event.target.files?.[0];
+            if (!file) {
+                setError("The PDF wasn't uploaded correctly.");
+                setIsLoading(false);
+                return;
+            }
+            await readResume(file);
+        } catch (error) {
+            setError('There was an error reading the resume. Please try again.');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            
+            {/* <div className="relative w-full flex flex-col items-center justify-center overflow-hidden rounded-md"> */}
+                {/* <SparklesCore
+                    id="tsparticlesfullpage"
+                    background="transparent"
+                    minSize={0.6}
+                    maxSize={1.4}
+                    particleDensity={100}
+                    className="absolute inset-0 w-full h-full"
+                    particleColor="#FFFFFF"
+                /> */}
+                <div
+                    className={`${styles.fileUploadBtnContainer} ${isDragOver ? styles.dragOver : ''}`}
+                    onDrop={(e: React.DragEvent<HTMLDivElement>) => handleDrop(e)}
+                    onDragOver={(e: React.DragEvent<HTMLDivElement>) => handleDragOver(e)}
+                    onDragEnter={(e: React.DragEvent<HTMLDivElement>) => handleDragEnter(e)}
+                    onDragLeave={(e: React.DragEvent<HTMLDivElement>) => handleDragLeave(e)}
+                >
+                    <input
+                        type="file"
+                        id="file-upload"
+                        onChange={handleButtonUpload}
+                        accept="application/pdf"
+                        hidden
+                    />
+                    <label htmlFor="file-upload" className={`${styles.label} ${styles.mainBtn}`}>
+                        <MdCloudUpload size={24} /> Upload resume
+                    </label>
+                </div>
+                {error && <p className={styles.errorMessage}>{error}</p>}
+            </div>
+        // </div>
+    );
+};
+
 export default ResumeUploader;
