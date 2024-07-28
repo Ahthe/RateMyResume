c0426fb68f5bc52b6f114c352121b295c5ea6edd
Abdul Arif <abdul.arif0705@gmail.com>
2024-07-27 21:32:51 -0400

    improved prompt for gpt4

--- app/api/resume/route.ts
+++ app/api/resume/route.ts
@@ -9,16 +9,20 @@ export async function POST(req: Request) {
   const { prompt } = await req.json();
  
   const response = mistral.chatStream({
-    model: 'mistral-large-latest',
-    messages: [{ 
-      role: 'user',
-      content: `CONTEXT: You are an expert at predicting the dollar worth of resumes.
-You are funny and witty, with an edge. You talk like a mentor hyping the user up.
-If the candidate is a man, you talk like a big brother, but still keep it a bit professional.
-If the candidate is a woman, you use talk in a sweet and funny way.
+    model: "mistral-large-latest",
+    messages: [
+      {
+        role: "user",
+        content: `You're an expert sofware engineer resume creator with 20 years of experience helping software engineers land jobs in big tech companies like Google, Apple, etc. Make sure the resume follows this XYZ principle that is compatible with the latest Applicant Tracking System (ATS). The XYZ principle I want you to follow is:
+         x what you built and what technologies you used
+         y what it accomplished
+         z measurements (percentage improvement, number of users, etc.)
+         e.g. Built a telehealth platform using React, Node.js, and Azure (X Point) enabling 800+ patient self-service features such as appointment booking, and symptom tracking, reducing time doctor spends on each patient by 30%
+         for all these points and don't just list responsibilities without metrics. Tell me which specific lines I should change because they don't follow xyz principle
+         You are tasked to give the user a reality check on their resume. 
 -------
 TASK: 
-- Analyze the resume given below and provide its estimated worth in US dollars. Give a single dollar value, not a range.
+- Analyze the resume given below and provide its estimated worth in percentage. Give a single percentage value, not a range.
 - Provide 4 short bullet points explanation of the key factors contributing to the assessment,
 and 4 tips on how they can improve their worth. Each bullet point should be less than 80 characters.
 - Write in a funny and witty way to make the response more engaging. If you can add 1 or 2 creative/funny metaphors, do that.
@@ -44,8 +48,9 @@ OUTPUT FORMAT:
       <li>...</li>
       ...
    </ul>
-</Improvements>`
-    }],
+</Improvements>`,
+      },
+    ],
   });
  
   const stream = MistralStream(response);
